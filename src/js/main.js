0ff51557e0010d2d8553fe12939eceb678c47332
Oleg <olegjan9@rambler.ru>
2022-02-15 14:00:27 +0300

    11.02.2022

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import modal from "./modules/modal";
 import slider from "./modules/slider";
+import forms from "./modules/forms";
 
 document.addEventListener('DOMContentLoaded', () => {
     "use strict";
     modal();
     slider();
+    forms();
 });
