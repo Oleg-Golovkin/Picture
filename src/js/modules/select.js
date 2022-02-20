46248df70c47510e3bece8437a7b3543dc9eb3aa
Oleg <olegjan9@rambler.ru>
2022-02-20 18:21:41 +0300

    20/02/2022

--- src/js/modules/select.js
+++ src/js/modules/select.js
@@ -1,5 +1,5 @@
 const select = () => {
-    const select = document.querySelectorAll("form#select"),
+    const select = document.querySelector("form#select"),
         totalPrice = document.querySelector(".calc-price");
 
     let num,
@@ -7,30 +7,37 @@ const select = () => {
         resMaterial,
         resOptions;
 
-  
-        select.addEventListener("input", (e) => {
-            if (e.target && e.target.matches("#size")) {
-                resSizes = e.target.value;
-            }
-            if (e.target && e.target.matches("#material")) {
-                resMaterial = e.target.value;
-            }
-            if (e.target && e.target.matches("#options")) {
-                resOptions = e.target.value;
-            }
+    document.querySelector(".promocode").addEventListener("input", (e) => {
+        console.log("fsd");
+        if (e.target.value == "IWANTPOPART") {
+           num = num * 0.3;
+        }
+    });
 
-            if (!resSizes || !resMaterial) {
-                totalPrice.innerHTML = "";
+
+    select.addEventListener("input", (e) => {
+        if (e.target && e.target.matches("#size")) {
+            resSizes = e.target.value;
+        }
+        if (e.target && e.target.matches("#material")) {
+            resMaterial = e.target.value;
+        }
+        if (e.target && e.target.matches("#options")) {
+            resOptions = e.target.value;
+        }
+
+        if (!resSizes || !resMaterial) {
+            totalPrice.innerHTML = "";
+        } else {
+            if (resSizes && resMaterial && resOptions) {
+                num = (+resSizes) + (+resMaterial) + (+resOptions);
+                totalPrice.innerHTML = `${num} рублей`;
             } else {
-                if (resSizes && resMaterial && resOptions) {
-                    num = (+resSizes) + (+resMaterial) + (+resOptions);
-                    totalPrice.innerHTML = `${num} рублей`;
-                } else {
-                    num = (+resSizes) + (+resMaterial);
-                    totalPrice.innerHTML = `${num} рублей`;
-                }
+                num = (+resSizes) + (+resMaterial);
+                totalPrice.innerHTML = `${num} рублей`;
             }
-        })
+        }
+    });
 
     document.querySelector(".promocode").addEventListener("input", (e) => {
         if (e.target.value == "IWANTPOPART") {
