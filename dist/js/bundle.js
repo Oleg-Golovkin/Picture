125199b9ea933a754dc6bc2cc6842f8ec2a3e235
Oleg <olegjan9@rambler.ru>
2022-02-22 22:56:13 +0300

    20/02/2022

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -78,65 +78,60 @@ const calculator = () => {
   //     optionsBlock = document.querySelector("#options"),
   //     promocodeBlock = document.querySelector(".promocode"),
   //     resultBlock = document.querySelector(".calc-price");
-  // function calculator(targetSelector) {
-  //     let num = (+sizeBlock) + (+materialBlock) + (+optionsBlock)
-  //     // if (e.target && e.target.matches("#size")) {
-  //     //     resSizes = e.target.value;
-  //     // }
-  //     // if (e.target && e.target.matches("#material")) {
-  //     //     resMaterial = e.target.value;
-  //     // }
-  //     // if (e.target && e.target.matches("#options")) {
-  //     //     resOptions = e.target.value;
-  //     // }
-  //     if (sizeBlock == "" || materialBlock.value == "") {
+  // const finish = () => {
+  //     let num = (Math.round(+sizeBlock.value)) + 
+  //     (Math.round(+materialBlock.value)) + 
+  //     (Math.round(+optionsBlock.value));
+  //     if (sizeBlock.value == "" || materialBlock.value == "") {
   //         resultBlock.innerHTML = "Выберите размер и материал картины";
   //     } else if (promocodeBlock.value == "IWANTPOPART") {
-  //         resultBlock.innerHTML = `${num * 0.3} рублей`;
+  //         num =  Math.round(num * 0.3);
+  //         resultBlock.innerHTML = `${num} рублей`;
   //     } else {            
   //         resultBlock.innerHTML = `${num} рублей`;
   //     }
-  // }
-  const select = document.querySelector("form#select"),
-        totalPrice = document.querySelector(".calc-price");
+  // };
+  // sizeBlock.addEventListener("change", finish);
+  // materialBlock.addEventListener("change", finish);
+  // optionsBlock.addEventListener("change", finish);
+  // promocodeBlock.addEventListener("input", finish);
+  const totalPrice = document.querySelector(".calc-price"),
+        promocodeBlock = document.querySelector(".promocode"),
+        select = document.querySelector("#select");
   let num, resSizes, resMaterial, resOptions;
-  select.addEventListener("change", e => {
-    if (e.target && e.target.matches("#size")) {
-      resSizes = e.target.value;
-    }
 
-    if (e.target && e.target.matches("#material")) {
-      resMaterial = e.target.value;
-    }
+  function resualt(trigger, event) {
+    trigger.addEventListener(event, e => {
+      if (e.target && e.target.matches("#size")) {
+        resSizes = e.target.value;
+      }
 
-    if (e.target && e.target.matches("#options")) {
-      resOptions = e.target.value;
-    }
+      if (e.target && e.target.matches("#material")) {
+        resMaterial = e.target.value;
+      }
 
-    if (!resSizes || !resMaterial) {
-      totalPrice.innerHTML = "";
-    } else {
-      if (resSizes && resMaterial && resOptions) {
+      if (e.target && e.target.matches("#options")) {
+        resOptions = e.target.value;
+      }
+
+      if (resOptions) {
         num = +resSizes + +resMaterial + +resOptions;
-        totalPrice.innerHTML = `${num} рублей`;
-        return num;
       } else {
         num = +resSizes + +resMaterial;
-        totalPrice.innerHTML = `${num} рублей`;
-        return num;
       }
-    }
-  });
-  const promocode = document.querySelector(".promocode");
-  promocode.addEventListener("input", e => {
-    if (e.target.value == "IWANTPOPART") {
-      console.log("fsdf");
-      totalPrice.innerHTML = `${num * 0.3} рублей`;
-      return num;
-    } else {
-      totalPrice.innerHTML = `${num} рублей`;
-    }
-  });
+
+      if (!resSizes || !resMaterial) {
+        totalPrice.innerHTML = "";
+      } else if (promocodeBlock.value == "IWANTPOPART") {
+        totalPrice.innerHTML = `${Math.round(num * 2)} рублей`;
+      } else {
+        totalPrice.innerHTML = `${num} рублей`;
+      }
+    });
+  }
+
+  resualt(select, "change");
+  resualt(promocodeBlock, "input");
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (calculator);
