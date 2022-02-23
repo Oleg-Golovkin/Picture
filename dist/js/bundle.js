1c925afe758c234200b624d400fc1489fe14596b
Oleg <olegjan9@rambler.ru>
2022-02-23 23:31:43 +0300

    23/02/2022

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -65,7 +65,30 @@ const calculator = () => {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 const filter = () => {
-  const choice = () => {};
+  const portfolioMenu = document.querySelector(".portfolioMenu-menu"),
+        portfolioWrapper = document.querySelector(".portfolio-wrapper"),
+        li = portfolioMenu.querySelectorAll("li"),
+        portfolio = document.querySelector("#portfolio"),
+        div = portfolioWrapper.querySelectorAll("div");
+
+  const choice = () => {
+    portfolio.addEventListener("click", e => {
+      li.forEach(li => {
+        li.classList.remove("active");
+      });
+      div.forEach(li => {
+        li.style.display = "none";
+      });
+
+      if (e.target && e.target.matches("li")) {
+        e.target.classList.add("active");
+        console.log(e.target.matches(".girl")); // div.forEach(li => {
+        // });
+      }
+    });
+  };
+
+  choice();
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (filter);
--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -1,7 +1,33 @@
 const filter = () => {
+    const
+        portfolioMenu = document.querySelector(".portfolioMenu-menu"),
+        portfolioWrapper = document.querySelector(".portfolio-wrapper"),
+        li = portfolioMenu.querySelectorAll("li"),
+        portfolio = document.querySelector("#portfolio"),
+        div = portfolioWrapper.querySelectorAll("div");
     const choice = () => {
-        
-    }
+        portfolio.addEventListener("click", (e) => {
+            li.forEach(li => {
+                li.classList.remove("active");
+            });
+            div.forEach(li => {
+                li.style.display = "none";
+            });
+
+            if (e.target && e.target.matches("li")) {
+                e.target.classList.add("active");
+                console.log(e.target.matches(".girl"));
+                // div.forEach(li => {
+               
+                // });
+            }
+
+        });
+    };
+
+    choice();
+
+
 }
 
 export default filter;
