22d43af2ee70f7727e04ee9b5b358ec5f0b319ce
Oleg <olegjan9@rambler.ru>
2022-02-23 17:43:09 +0300

    23/02/2022

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ const webpack = require('webpack-stream');
 const path = require('path');
 
 // const dist = './dist/';
-const dist = 'C:/MAMP/htdocs/';
+const dist = 'C:/MAMP/htdocs/'; 
 // const dist = '/Applications/MAMP/htdocs/';
 
 //1. Настраиваем обновление страницы
