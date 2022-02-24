967b0bc1ffeca6534cdd57ff8b25719334f7839d
Oleg <olegjan9@rambler.ru>
2022-02-24 14:15:55 +0300

    24.02.2022

--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -1,56 +1,57 @@
 const filter = () => {
     const
+        // Оболочка для кнопок
         portfolioMenu = document.querySelector(".portfolioMenu-menu"),
-        portfolioWrapper = document.querySelector(".portfolio-wrapper"),
+        // Все кнопки
         li = portfolioMenu.querySelectorAll("li"),
-        portfolio = document.querySelector("#portfolio"),
-        div = portfolioWrapper.querySelectorAll("div"),
-        girl = portfolioWrapper.querySelectorAll(".girl"),
-        lovers = portfolioWrapper.querySelectorAll(".lovers"),
-        guy = portfolioWrapper.querySelectorAll(".guy"),
-        chef = portfolioWrapper.querySelectorAll(".chef");
+        // Облочка вызываемого контента
+        portfolioWrapper = document.querySelector(".portfolio-wrapper"),
+        // Весь вызываемый контент    
+        div = portfolioWrapper.querySelectorAll("div");
 
 
-
-    const choice = () => {
-        // let selectors = portfolioWrapper.querySelectorAll();
-        portfolio.addEventListener("click", (e) => {
+    // Особенности фукнции.
+    // 1. На каждую нажимаемую кнопку поставил событие.
+    // slectorBtn - селектор кнопки.
+    // Соответственно, сколько вызвал функций, столько и
+    // кнопок.
+    // 2. Для каждой кнопки показ опеределнного контента
+    // slectorContent
+    // Значит нажимаю одну кнопку slectorBtn показывается
+    // один конент slectorContent, нажимаю другую (другая
+    // фукнция) другой контент.
+    const choice = (slectorBtn, slectorContent) => {
+        // Кнопка
+        let blockBtn = document.querySelector(slectorBtn),
+            // Показываемый контент
+            blockContent = document.querySelectorAll(slectorContent);
+        blockBtn.addEventListener("click", (e) => {
+            // Сникаю класс активности у всех кнопок по умолчанию
             li.forEach(li => {
                 li.classList.remove("active");
             });
+            // Скрываю весь контент по умолчнию
             div.forEach(li => {
                 li.style.display = "none";
             });
-
-            if (e.target.matches(".lovers")) {
-                e.target.classList.add("active");
-                lovers.forEach(selector => {
-                    selector.style.display = "block";
-                });
-            }
-            if (e.target.matches(".girl")) {
-                e.target.classList.add("active");
-                girl.forEach(selector => {
-                    selector.style.display = "block";
-                });
-            }
-            if (e.target.matches(".guy")) {
-                e.target.classList.add("active");
-                guy.forEach(selector => {
-                    selector.style.display = "block";
-                });
-            }
-            if (e.target.matches(".chef")) {
-                e.target.classList.add("active");
-                chef.forEach(selector => {
-                    selector.style.display = "block";
-                });
-            }
+        //     Показываю класс активности у нажимаемой кнопки
+        //     (поэтому для каждой кнопки совя фукнция и поэтому
+        // не стал делать через делигирование события)
+            e.target.classList.add("active");
+            // Показываю тот блок, которых задал в функции в качестве
+            // второго параметра
+            blockContent.forEach(item => {
+                item.style.display = "block";
+            });
         });
     };
 
-    choice();
-  
+    choice(".portfolioMenu-menu .girl", ".portfolio-wrapper .girl");
+    choice(".portfolioMenu-menu .lovers", ".portfolio-wrapper .lovers");
+    choice(".portfolioMenu-menu .guy", ".portfolio-wrapper .guy");
+    choice(".portfolioMenu-menu .chef", ".portfolio-wrapper .chef");
+
+
 };
 
 export default filter;
