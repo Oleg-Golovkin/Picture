9f3e28d225aa4414cb29b7aa64d07ed85274efa7
Oleg <olegjan9@rambler.ru>
2022-02-27 09:44:53 +0300

    27.02.2022

--- src/js/modules/sizeBlock.js
+++ src/js/modules/sizeBlock.js
@@ -1,18 +1,34 @@
 const sizeBlock = () => {
-    const sizeWrapper = document.querySelector(".sizes-wrapper"),
-        img = sizeWrapper.querySelectorAll("img"),
-        p = sizeWrapper.querySelectorAll("p")
+    const sizesBlocks = document.querySelectorAll(".sizes-block");
 
-    const showImg = () => {
-        sizeWrapper.addEventListener("mouseover", (e) => {
-            if (e.target && e.target.matches("img")) {
-                e.target.src = "assets/img/sizes-1-1.png";
-                p.forEach(p => {
-                    p.style.display = "none";
-                });               
-            }
-        });
+
+    const showImg = (target, sizesBlock) => {
+        if (target && target.matches("img")) {
+            let src = target.src
+            target.src = src.slice(0, -4) + "-1.png"
+            const p = sizesBlock.querySelectorAll("p");
+            p.forEach(p => {
+                p.style.display = "none";
+            });
+        }
     };
-    showImg();
+
+    const hideImg = (target, sizesBlock) => {
+        if (target && target.matches("img")) {
+            let src = target.src
+            target.src = src.slice(0, -6) + ".png"
+            const p = sizesBlock.querySelectorAll("p");
+            p.forEach(p => {
+                p.style.display = "block";
+            });
+        }
+    };
+    
+    sizesBlocks.forEach(sizesBlock=> {
+        sizesBlock.addEventListener("mouseover", (e) => {
+            showImg(e.target, sizesBlock);
+        });
+    })
+    
 };
 export default sizeBlock;
