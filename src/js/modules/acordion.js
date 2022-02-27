e05be35e2f39d7f7889a0cc9275596ac76bd42f9
Oleg <olegjan9@rambler.ru>
2022-02-27 17:13:45 +0300

    27/02/2022

---
+++ src/js/modules/acordion.js
@@ -0,0 +1,35 @@
+const acordion = () => {
+    const accordionHeadings = document.querySelectorAll(".accordion-heading"),
+        accordionBlocks = document.querySelectorAll(".accordion-block");
+
+    // 1. Весь акордион закрыт
+    function hideAccordionBlock() {
+        accordionBlocks.forEach(accordionBlock => {
+            accordionBlock.style.display = "none";
+        });
+    }
+    hideAccordionBlock();
+    // 2. Открытие по соответствующему индексу
+    function showAccordionBlock(i) {
+        accordionBlocks[i].style.display = "block";
+        accordionBlocks[i].classList.add("animate__animated", "animate__fadeInDown");
+    }
+
+    accordionHeadings.forEach((accordionHeading, i) => {
+        accordionHeading.addEventListener("click", (e) => {
+            hideAccordionBlock();
+            showAccordionBlock(i);
+        });
+    });
+    // При нажатии на пустое пространство (!e.target.matches("span") все дочерние, кроме span)
+    // аккордион схлопывается
+    document.querySelector(".often-questions").addEventListener("click", (e) => {
+        if (!e.target.matches("span")) {
+            hideAccordionBlock();
+        }
+    });
+
+
+
+};
+export default acordion;
--- src/js/modules/sizeBlock.js
+++ src/js/modules/sizeBlock.js
@@ -2,36 +2,33 @@ const sizeBlock = () => {
     const sizesBlocks = document.querySelectorAll(".sizes-block");
 
 
-    const showImg = (target, sizesBlock) => {
-        if (target && target.matches("img")) {
-            let src = target.src
-            target.src = src.slice(0, -4) + "-1.png"
-            const p = sizesBlock.querySelectorAll('p:not(.sizes-hit)');
-            p.forEach(p => {
-                p.style.display = "none";
-            });
-        }
+    const showImg = (sizesBlock) => {
+        const img = sizesBlock.querySelector("img");
+        console.log(img.src);
+        img.src = img.src.slice(0, -4) + "-1.png";
+        const p = sizesBlock.querySelectorAll('p:not(.sizes-hit)');
+        p.forEach(p => {
+            p.style.display = "none";
+        });
     };
 
-    const hideImg = (target, sizesBlock) => {
-        if (target && target.matches("img")) {
-            let src = target.src
-            target.src = src.slice(0, -6) + ".png";
-            const p = sizesBlock.querySelectorAll('p:not(.sizes-hit)');
-            p.forEach(p => {
-                p.style.display = "block";
-            });
-        }
+    const hideImg = (sizesBlock) => {
+        let img = sizesBlock.querySelector("img");
+        img.src = img.src.slice(0, -6) + ".png";
+        const p = sizesBlock.querySelectorAll('p:not(.sizes-hit)');
+        p.forEach(p => {
+            p.style.display = "block";
+        });
     };
-    
-    sizesBlocks.forEach(sizesBlock=> {
+
+    sizesBlocks.forEach(sizesBlock => {
         sizesBlock.addEventListener("mouseover", (e) => {
-            showImg(e.target, sizesBlock);
+            showImg(sizesBlock);
         });
         sizesBlock.addEventListener("mouseout", (e) => {
-            hideImg(e.target, sizesBlock);
+            hideImg(sizesBlock);
         });
-    })
-    
+    });
+
 };
 export default sizeBlock;
